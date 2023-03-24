f55b680182e4ec5e0b540baea5bd2355c9075985
Vitor Bahir <vitorb82014@gmail.com>
2023-03-24 03:57:50 -0300

    Initial implementation

---
+++ Entities/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exception2_csharp.Entities.Exceptions
+{
+    internal class DomainException : Exception
+    {
+        public DomainException(string message) : base(message) { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using exception2_csharp.Entities;
+using exception2_csharp.Entities.Exceptions;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account acc = new Account(number, holder, balance, withdrawLimit);
+
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        try
+        {
+            acc.WithDraw(amount);
+            Console.WriteLine("New Balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+        catch (DomainException e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+    }
+}
